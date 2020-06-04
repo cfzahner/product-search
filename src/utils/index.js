ef08b1af85cf552d4ac63ea05759790ded48c02a
Manav Misra <manavm@visionwebsoft.com>
2020-06-04 18:30:10 -0500

    :fire: Remove unused utility fxn

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,2 @@
 export const parseDollarPrice = (dollarPrice) =>
   parseFloat(dollarPrice.slice(1));
- * @param {Array} arr
- * @returns {Array}
- */
-export const removeDuplicates = (arr) => [...new Set(arr)];
