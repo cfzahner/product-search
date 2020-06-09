84271d32575eec1ff4849daa7522ae87344bdec0
Manav Misra <manavm@visionwebsoft.com>
2020-06-09 18:59:47 -0500

    :lipstick: Add styles for table

--- src/components/FilterableProductTable/ProductTable.js
+++ src/components/FilterableProductTable/ProductTable.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+import "./ProductTable.css";
+
 const renderTBody = (products) => {
   return products.map(({ name, price }, index) => {
     return (
@@ -15,6 +17,12 @@ const renderTBody = (products) => {
 export const ProductTable = ({ products }) => {
   return (
     <table>
+      <thead>
+        <tr>
+          <th>Product Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
       <tbody>{renderTBody(products)}</tbody>
     </table>
   );
