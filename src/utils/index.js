158ebf58799b2bb6e982a4fc48a78174e73482f7
Manav Misra <manavm@visionwebsoft.com>
2020-06-20 07:37:10 -0500

    :fire: Remove Proxy from 'utils'
    Missing from earlier commit.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,2 @@
-// TODO: Consider scoping 'react only' utils
-export const createStateProxy = (component) =>
-  new Proxy(component, {
-    // 'Trap' the object on which we attempted to change a 'prop' - 'comp'
-    set(comp, prop, value) {
-      // 'this' is scoped to 'set' - so we must 'comp' which will be the original 'this'
-      // Use brackets to interpolate - 'prop'
-      comp.setState({ [prop]: value });
-      return true;
-    },
-  });
-
 export const parseDollarPrice = (dollarPrice) =>
   parseFloat(dollarPrice.slice(1));
